71696f8b5cb02dbb97157810b58114f6d490dd0e
skoved <sam.koved@gmail.com>
2016-12-05 18:27:38 -0500

    added createTable

--- driver.cpp
+++ driver.cpp
@@ -6,14 +6,19 @@
 
 using namespace std;
 
-int main(int argc, char** argv){
-    //read input and setup for kmp
-	ifstream infile(argv[1]);
+int main(int argc, char** argv){//read input and setup for kmp
+	ifstream infile;
 	vector<string> lines;
 	string line;
-	while(getline(infile, line)){
-		lines.push_back(line);
-	}
+    infile.open(argv[1]);
+    if(!infile){
+        lines.push_back(argv[1]);
+    }
+    else{
+        while(getline(infile, line)){
+            lines.push_back(line);
+        }
+    }
 	
 	return 0;
 }
